6c5324158a01ce099e96f2eb6569ce528ad7734a
Pyotr Ryzhov <maestro_pete@inbox.ru>
2021-11-22 09:22:48 +0300

    fixed script structure

--- releaseAnchoredObjects.jsx
+++ releaseAnchoredObjects.jsx
@@ -1,18 +1,46 @@
-
-const SCRIPT_NAME = "TestScript";
+const SCRIPT_NAME = "releaseAnchoredObjects";
 const SCRIPT_VERSION = 0.1;
 
 // ============== MAIN
 function main() {
   if (!app.documents.length) {
     exit();
-    };
+  }
 
-  var myDoc = app.activeDocument;
-  var anchoredOjbects = myDoc.anchoredOjbects.length;
-  alert(anchoredOjbects.length);
-}
-//=============== END OF MAIN
+  if (app.selection.length) {
+    for (var s = 0; s < app.selection.length; s++) {
+      var selectedItem = app.selection[s];
+      if (
+        selectedItem.constructor.name == "Word" ||
+        selectedItem.constructor.name == "Text"
+      ) {
+        if (
+          selectedItem.textFrames.length ||
+          selectedItem.rectangles.length
+        ) {
+          alert(selectedItem.textFrames.length + "\n" + selectedItem.rectangles.length);
+          for (tf in selectedItem.textFrames) {
+
+          }
+        }
+      }
+    }
+
+    // for (s in app.selection) {
+    // }
+  } else {
+  }
+  // var myDoc = app.activeDocument;
+  // var myStories = myDoc.stories;
+  // var myParents = [];
+  //   myParents.push(myStories[8].parent.constructor.name);
+  //=============== END OF MAIN
 }
 
-app.doScript(main, ScriptLanguage.JAVASCRIPT, undefined, UndoModes.entireScript, SCRIPT_NAME);
+app.doScript(
+  main,
+  ScriptLanguage.JAVASCRIPT,
+  undefined,
+  UndoModes.entireScript,
+  SCRIPT_NAME
+);
