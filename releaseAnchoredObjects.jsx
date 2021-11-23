22988d9028ec183d2e7e31261431a8118e8ef660
Pyotr Ryzhov <maestro_pete@inbox.ru>
2021-11-23 12:16:13 +0300

    added Options obj

--- releaseAnchoredObjects.jsx
+++ releaseAnchoredObjects.jsx
@@ -1,3 +1,9 @@
+// TODO: []
+// TODO: ui
+//
+//
+#target indesign
+
 const SCRIPT_NAME = "releaseAnchoredObjects";
 const SCRIPT_VERSION = 0.1;
 
@@ -5,6 +11,19 @@ const SCRIPT_VERSION = 0.1;
 function main() {
   if (!app.documents.length) {
     exit();
+  }
+
+  const Options = {
+    leaveTextFramesOnly: true,
+  };
+
+  function releaseObj(obj) {
+    var parentPage = obj.parentPage;
+    var parentSpread = parentPage.parent;
+    var doubleObj = obj.duplicate(parentSpread, [0, 0]);
+    doubleObj.fit(FitOptions.FRAME_TO_CONTENT);
+    doubleObj.select();
+    obj.remove();
   }
 
   if (app.selection.length) {
@@ -18,16 +37,21 @@ function main() {
           selectedItem.textFrames.length ||
           selectedItem.rectangles.length
         ) {
-          alert(selectedItem.textFrames.length + "\n" + selectedItem.rectangles.length);
-          for (tf in selectedItem.textFrames) {
+          for (var tf = 0; tf < selectedItem.textFrames.length; tf++) {
+            releaseObj(selectedItem.textFrames[tf]);
+          }
 
+          for (var rc = 0; rc < selectedItem.rectangles.length; rc++) {
+            if (!Options.leaveTextFramesOnly) {
+              releaseObj(selectedItem.rectangles[rc])
+            }
+            else {
+              selectedItem.rectangles[rc].remove();
+            }
           }
         }
       }
     }
-
-    // for (s in app.selection) {
-    // }
   } else {
   }
   // var myDoc = app.activeDocument;
